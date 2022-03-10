ba12c99c6f4d8b0dd615102d3dd84c5b50272ca6
emmaprivora <100583627+emmaprivora@users.noreply.github.com>
2022-03-10 13:21:20 +0100

    Jobbat lite på Client :)

--- src/client/controller/ClientController.java
+++ src/client/controller/ClientController.java
@@ -1,38 +1,54 @@
 package client.controller;
 
 import client.view.MainFrame;
+import model.User;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import javax.sound.sampled.Port;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.Socket;
 
 public class ClientController {
     private MainFrame view;
+    private Socket socket;
+    private int port;
+    private String ip;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
+    private User user;
 
     public ClientController(){
         view = new MainFrame(this);
+        this.ip = "127.0.0.1";
+        this.port = 721;
+    }
+
+    public void connect() throws IOException {
+        socket = new Socket(ip, port);
+        ois = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+        oos = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+        oos.writeObject(user);
+    }
+
+    public void reConnect() {
 
     }
 
+    public void disconnect() throws IOException {
+        socket.close();
+    }
 
-
-    private class Client extends Thread{
+    private class Client extends Thread {
         private ObjectInputStream ois;
         private ObjectOutputStream oos;
 
         //Körs när client startar
         public void run(){
-            try{
-                oos.writeObject("g"); //Vid anslutning identifierar sig användaren med användarnamn och bild.
-
-
-            } catch (Exception e) {
-                    e.printStackTrace();
-            }
-
-            while(true){
                 try{
-                    oos.writeObject("g"); //Vänta på att anv skriver ett meddelande.
-
+                    while(true) {
+                        
+                        oos.writeObject("g"); //Vänta på att anv skriver ett meddelande.
+                    }
 
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -40,7 +56,9 @@ public class ClientController {
             }
 
         }
-    }
+
+
+
 
 
 
