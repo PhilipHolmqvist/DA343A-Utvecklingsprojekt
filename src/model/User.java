18fbb214f7c12428073908c85ab9de3ebb25d46d
atonix5 <philip_holmqvist@hotmail.com>
2022-03-10 10:19:39 +0100

    User

--- src/model/User.java
+++ src/model/User.java
@@ -1,6 +1,28 @@
 package model;
 
+import javax.swing.*;
+
 public class User {
     private String username;
+    private Icon icon;
 
+    public User(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Icon getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Icon icon) {
+        this.icon = icon;
+    }
 }
