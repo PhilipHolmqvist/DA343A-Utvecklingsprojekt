0aa0413e187020f6a7bf959f08b6b5483ad6b92e
emmaprivora <100583627+emmaprivora@users.noreply.github.com>
2022-03-17 17:26:49 +0100

    Login-ruta poppar upp med vissa fel (fel på file pathen?)

--- src/client/controller/ClientController.java
+++ src/client/controller/ClientController.java
@@ -1,21 +1,44 @@
 package client.controller;
 
+import client.view.LoginWindow;
 import client.view.MainFrame;
 import model.Buffer;
 import model.Message;
 import model.User;
 
 import javax.sound.sampled.Port;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
 
 public class ClientController {
     private MainFrame view;
+    private LoginWindow loginWindow;
+    private User login;
 
     public ClientController() throws IOException {
-        view = new MainFrame(this);
-        new ServerConnection("127.0.0.1", 721, this);
+        LoginWindow loginWindow = new LoginWindow(new Frame("Jdialog demo"));
+        loginWindow.setVisible(true);
+
+        if (loginWindow.authenticate()) {
+            String username = loginWindow.getUsername();
+            String imagePath = loginWindow.getImagePath();
+            login = new User(username, new ImageIcon(imagePath));
+
+            view = new MainFrame(this);
+
+            String server = loginWindow.getServerName();
+            int port = loginWindow.getPort();
+            new ServerConnection("127.0.0.1", 721, this);
+
+            login.setUsername(loginWindow.getUsername());
+            //login.setIcon(få ikon här???);
+        } else {
+            System.exit(0);
+        }
+
 
     }
 
