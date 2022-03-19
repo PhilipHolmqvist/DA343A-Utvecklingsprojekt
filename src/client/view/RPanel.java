ee50f52decb362edba4548400c5b0f0ce9bdf8a7
emmaprivora <100583627+emmaprivora@users.noreply.github.com>
2022-03-19 13:04:30 +0100

    fixat client view lite (ej klar)

--- src/client/view/RPanel.java
+++ src/client/view/RPanel.java
@@ -7,12 +7,40 @@ import java.awt.*;
 
 public class RPanel extends JPanel {
     private ClientController controller;
+    private int width;
+    private int height;
+    private JTextArea chatWindow;
+    private JTextArea writeMessageWindow;
+    private JButton sendMessage;
 
 
-    public RPanel(ClientController controller, int widht, int hight){
+    public RPanel(ClientController controller, int width, int height){
         this.controller = controller;
-        this.setSize(widht, hight);
+        this.setSize(width, height);
+        this.setLayout(null);
+        setUp();
+    }
 
-        this.setBackground(Color.black);
+    private void setUp() {
+        chatWindow = new JTextArea();
+        chatWindow.setLocation(40, 40);
+        chatWindow.setSize(500, 400);
+        chatWindow.setBorder(BorderFactory.createLoweredBevelBorder());
+        chatWindow.setBackground(new Color(255, 255, 255));
+        chatWindow.setEditable(false);
+        add(chatWindow);
+
+        writeMessageWindow = new JTextArea();
+        writeMessageWindow.setLocation(40, 450);
+        writeMessageWindow.setSize(500, 90);
+        writeMessageWindow.setBorder(BorderFactory.createLoweredBevelBorder());
+        writeMessageWindow.setBackground(new Color(255, 255, 255));
+        writeMessageWindow.setEditable(true);
+        add(writeMessageWindow);
+
+        sendMessage = new JButton("Skicka meddelande");
+        sendMessage.setSize(150, 40);
+        sendMessage.setLocation(390,560);
+        add(sendMessage);
     }
 }
