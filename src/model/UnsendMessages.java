f2c1ff4c5958b41ac80eddf88763c98e8e16f0a7
atonix5 <philip_holmqvist@hotmail.com>
2022-03-22 17:30:02 +0100

    lite skit..

--- src/model/UnsendMessages.java
+++ src/model/UnsendMessages.java
@@ -1,4 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class UnsendMessages {
+    private HashMap<User, ArrayList<Message>> unsedMessages;
+
+    public UnsendMessages(){
+        this.unsedMessages = new HashMap<>();
+    }
+
+    public synchronized void put(User user, Message message){
+        if(unsedMessages.get(user) == null){
+            ArrayList<Message> unsedMsg = new ArrayList<>();
+            unsedMessages.put(user, unsedMsg);
+        }
+        ArrayList<Message> unsed = unsedMessages.get(user);
+        unsed.add(message);
+        unsedMessages.replace(user, unsed);
+    }
+
+    public synchronized ArrayList<Message> get(User user){
+        ArrayList<Message> unsentMsgsToUser = unsedMessages.get(user);
+        return unsentMsgsToUser;
+    }
+
 }
